7cd710741e37bfe1026210ebd6244a6b67ac751a
huiwang <hui.wang@accuenergy.com.cn>
2020-04-23 11:38:44 +0800

    spring secuity 系列教程代码

---
+++ src/test/java/com/example/springsecurityall/SpringSecurityAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
