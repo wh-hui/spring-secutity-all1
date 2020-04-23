9d306bab998d790936fbdf557b1edd54c759667a
huiwang <hui.wang@accuenergy.com.cn>
2020-04-23 11:47:10 +0800

    Spring Security 实战干货：用户信息UserDetails相关入门

---
+++ springs-ecurity-demo1/src/main/java/com/example/springsecuritydemo1/SpringsEcurityDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsEcurityDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsEcurityDemo1Application.class, args);
+    }
+
+}
---
+++ springs-ecurity-demo1/src/test/java/com/example/springsecuritydemo1/SpringsEcurityDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsEcurityDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
