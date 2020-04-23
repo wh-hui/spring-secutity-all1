5d2f424d41ed720b485c305001597f3012a5acbf
huiwang <hui.wang@accuenergy.com.cn>
2020-04-23 13:36:16 +0800

    Spring Security 实战干货：用户信息UserDetails相关入门

---
+++ springs-ecurity-demo1/src/main/java/com/example/springsecuritydemo1/test/test.java
@@ -0,0 +1,15 @@
+package com.example.springsecuritydemo1.test;
+
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * ClassName: test
+ * Description: TODO
+ * Author: Administrator
+ * Date: 2020/4/23 0023 13:19
+ */
+public class test {
+    UserDetailsService userDetailsService;
+    UserDetailsServiceAutoConfiguration userDetailsServiceAutoConfiguration;
+}
